f4c6d8b1c1011972537b8840f918a621dd7feaca
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-06-26 11:39:03 +0100

    Update main.py keep it pg

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ Seeds = 0
 Seeds = Trees * TreeSpeed
 Check1 = 0
 TreeMutation1 = 0
+limit = 1000000
 
-
-while Trees < 1000:
+while Trees < limit:
+  Seeds = Trees * TreeSpeed
+  print(f"seeds: {Seeds}")
   Check1 = random.randint(0, 3)
   if Check1 == 2:
-  TreeMutation1 = random.randint(0, 5)
-  TreeMutation1 = TreeMutation1 / 100
-  TreeHardness -= TreeMutation1
+    TreeMutation1 = random.randint(0, 2) / 100
+    TreeHardness -= TreeMutation1
   Trees = Seeds
   Seeds = 0
-  print(Seeds)
-  print(Trees)
+  print(f"trees: {Trees}")
 
+print(f"Reached limit: {limit}")
 sys.exit
