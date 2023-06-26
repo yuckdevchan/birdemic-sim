70094e7660f265545b1d65d920d028c14e72c6b8
manSlayer16 <120093141+manSlayer16@users.noreply.github.com>
2023-06-26 11:21:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ TreeHardness = 1
 TreeSpeed = TreeEnergy / TreeHardness
 Seeds = 0
 Seeds = Trees * TreeSpeed
-
+Check1 = randint(0,3)
+if Check1 == 2:
+  treeMutation1 = random.random(0,0.01)
 
 
 while Trees < 1000:
