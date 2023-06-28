4f83fde90545a167fd120df87c7830431c71727f
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-06-28 10:27:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,16 @@ Seeds = Trees * TreeSpeed
 Check1 = 0
 TreeMutation1 = 0
 limit = 1000000
+Birds = 10
+BirdSpeed = 3
+BirdEat = Birds * BirdSpeed
+BirdAllEat = 10
 
 while Trees < limit:
   Seeds = Trees * TreeSpeed
+  BirdEat = Birds * BirdSpeed
+  Seeds -= BirdEat
+  print(f"trees: {Trees}")
   print(f"seeds: {Seeds}")
   Check1 = random.randint(0, 3)
   if Check1 == 2:
@@ -20,7 +27,7 @@ while Trees < limit:
     TreeHardness -= TreeMutation1
   Trees = Seeds
   Seeds = 0
-  print(f"trees: {Trees}")
+  
 
 print(f"Reached limit: {limit}")
 sys.exit
